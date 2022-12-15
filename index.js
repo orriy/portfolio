7dcd51afd559fc516cb625a3239ea804b08f9454
orriy <jeonghuigim63@gmail.com>
2022-12-15 16:25:02 +0900

    portfolio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.addEventListener("scroll", function () {
   const scrollTop = document.documentElement.scrollTop;
+
   const height =
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
@@ -11,4 +12,17 @@ window.addEventListener("scroll", function () {
       width :${40 + scrollWidth * 5}vw;
       height :${30 + scrollWidth * 5}vh
     `;
+});
+
+var swiper = new Swiper(".slide1", {
+  spaceBetween: 100,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".pg1",
+    clickable: true,
+  },
 });
